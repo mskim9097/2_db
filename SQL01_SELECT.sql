da86bc8f55c10e66f30fa98575fe814f1555ff11
MINSU KIM <mskim9097@gmail.com>
2023-01-11 15:16:03 +0900

    20230111 commit

--- SQL01_SELECT.sql
+++ SQL01_SELECT.sql
@@ -161,7 +161,7 @@ WHERE COACH_PROFESSOR_NO IS NULL;
 -- 단, 이때 출력 화면의 헤더는 "년도", "년도 별 평점" 이라고 찍히게 하고,
 -- 점수는 반올림하여 소수점 이하 한 자리까지만 표시한다.
 
-SELECT SUBSTR(TERM_NO, 1, 4) "년도", round(AVG(POINT), 1)  "년도 별 평점"
+SELECT SUBSTR(TERM_NO, 1, 4) "년도", ROUND(AVG(POINT), 1)  "년도 별 평점"
 FROM TB_GRADE
 WHERE STUDENT_NO = 'A112113'
 GROUP BY SUBSTR(TERM_NO, 1, 4)
@@ -192,3 +192,75 @@ FROM TB_GRADE
 WHERE STUDENT_NO = 'A112113'
 GROUP BY SUBSTR(TERM_NO, '1', '4'), SUBSTR(TERM_NO, '5', '2')
 ORDER BY SUBSTR(TERM_NO, '1', '4');
+
+----------------------------------------------------------------------------------------
+--[Additional SELECT - Option]
+
+-- 1. 학생이름과 주소지를 표시하시오.
+-- 단, 출력 헤더는 "학생 이름", "주소지"로 하고, 정렬은 이름으로 오름차순 표시하도록 한다.
+
+SELECT STUDENT_NAME "학생 이름", STUDENT_ADDRESS "주소지"
+FROM TB_STUDENT
+ORDER BY 1;
+
+-- 2. 휴학중인 학생들의 이름과 주민번호를 나이가 적은 순서로 화면에 출력하시오.
+
+SELECT STUDENT_NAME, STUDENT_SSN
+FROM TB_STUDENT
+WHERE ABSENCE_YN = 'Y'
+ORDER BY STUDENT_SSN DESC;
+
+-- 3. 주소지가 강원도나 경기도인 학생들 중 1900년대 학번을 가진 학생들의 이름과 학번,
+-- 주소를 이름의 오름차순으로 화면에 출력하시오.
+-- 단, 출력헤더에는 "학생이름", "학번", "거주지 주소" 가 출력되도록 한다.
+
+SELECT STUDENT_NAME "학생이름", STUDENT_NO "학번", STUDENT_ADDRESS "거주지 주소", ENTRANCE_DATE
+FROM TB_STUDENT
+WHERE (STUDENT_ADDRESS LIKE '%강원%' OR STUDENT_ADDRESS LIKE '%경기%')
+AND ENTRANCE_DATE < '2000-01-01'
+ORDER BY STUDENT_NAME;
+
+-- 4. 현재 법학과 교수 중 가장 나이가 많은 사람부터 이름을 확인할 수 있는 SQL문장을 작성하시오.
+-- (법학과의 '학과코드'는 학과 테이블(TB_DEPARTMENT)를 조회해서 찾아내도록 하자)
+
+SELECT PROFESSOR_NAME, PROFESSOR_SSN
+FROM TB_PROFESSOR
+JOIN TB_DEPARTMENT USING (DEPARTMENT_NO) 
+WHERE DEPARTMENT_NAME = '법학과'
+ORDER BY 2;
+
+
+-- 5. 2004년 2학기에 'C3118100' 과목을 수강한 학생들의 학점을 조회하려고 한다.
+-- 학점이 높은 학생부터 표시하고, 학점이 같으면 학번이 낮은 학생부터 표시하는 구문을 작성해보시오.
+
+SELECT STUDENT_NO, POINT
+FROM TB_STUDENT
+JOIN TB_GRADE USING (STUDENT_NO)
+WHERE TERM_NO = '200402' AND CLASS_NO = 'C3118100'
+ORDER BY POINT DESC;
+
+
+-- 6. 학생 번호, 학생 이름, 학과 이름을 학생 이름으로 오름차순 정렬하여 출력하는 SQL 문을 작성하시오.
+
+SELECT STUDENT_NO, STUDENT_NAME, DEPARTMENT_NAME
+FROM TB_STUDENT
+JOIN TB_DEPARTMENT USING (DEPARTMENT_NO);
+--ORDER BY STUDENT_NAME;
+
+-- 7. 춘 기술대학교의 과목 이름과 과목의 학과 이름을 출력하는 SQL 문장을 작성하시오.
+
+SELECT CLASS_NAME, DEPARTMENT_NAME
+FROM TB_CLASS
+JOIN TB_DEPARTMENT USING (DEPARTMENT_NO);
+
+
+-- 8. 과목별 교수 이름을 찾으려고 한다.
+-- 과목 이름과 교수 이름을 출력하는 SQL 문을 작성하시오.
+
+SELECT CLASS_NAME, PROFESSOR_NAME
+FROM TB_CLASS
+JOIN TB_CLASS_PROFESSOR USING (CLASS_NO)
+JOIN TB_PROFESSOR USING (PROFESSOR_NO);
+
+
+
