cec633642b590b421f438ce365988c1f1db2c608
MINSU KIM <mskim9097@gmail.com>
2023-02-06 15:16:36 +0900

    20230206 commit

---
+++ JDBC SYS계정.sql
@@ -0,0 +1,15 @@
+-- SYS 관리자 계정
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+
+-- 사용자 계정 생성
+CREATE USER member_ms IDENTIFIED BY member1234;
+
+-- 생성한 사용자 계정에 권한 부여
+GRANT CONNECT, RESOURCE, CREATE VIEW TO member_ms;
+
+
+-- 테이블 스페이스 할당
+ALTER USER member_ms DEFAULT
+TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
